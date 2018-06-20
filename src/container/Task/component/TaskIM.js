c7e08baf888cccc65afa2b7f54b221cf96242702
RaymanNg <touhourayman@gmail.com>
2018-06-20 15:20:30 +0800

    add more IM code

--- src/container/Task/component/TaskIM.js
+++ src/container/Task/component/TaskIM.js
@@ -1,34 +1,113 @@
-import { ChatFeed, Message } from 'react-chat-ui';
-import React, { Component } from 'react';
+import {ChatFeed, Message} from 'react-chat-ui';
+import React, {Component} from 'react';
+import {List, Avatar, Input, Button, Layout} from 'antd';
+import styled from 'styled-components';
 
-class TaskCreate extends Component {
-    state = {
-      messages: [
-        new Message({ id: 1, message: 'Hey!' }),
-        new Message({ id: 1, message: 'I forgot to mention...' }),
-      ],
-    };
+const {Header, Footer, Sider, Content} = Layout;
+const {TextArea} = Input;
 
+const Container = styled.div`
+  background-color: #FFF;
+  margin-top: 20px;
+  padding: 20px;
+`;
+
+const MessageButton = styled(Button)`
+  float: right;
+  margin-top: 10px;
+`;
+
+const contactData = [
+    {
+        title: '张三',
+    },
+    {
+        title: '李四',
+    },
+    {
+        title: '王五',
+    },
+    {
+        title: '赵六',
+    },
+];
+
+class ContactList extends Component {
     render() {
-      return (
-        <ChatFeed
-          messages={this.state.messages}
-          hasInputField={false}
-          bubblesCentered={false}
-          bubbleStyles={
-                    {
-                        text: {
-                            fontSize: 30,
-                        },
-                        chatbubble: {
-                            borderRadius: 70,
-                            padding: 40,
-                        },
-                    }
-                }
-        />
-      );
+        return (
+            <List
+                itemLayout="horizontal"
+                dataSource={contactData}
+                renderItem={item => (
+                    <List.Item>
+                        <List.Item.Meta
+                            avatar={<Avatar
+                                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                            title={<Button>{item.title}</Button>}
+                        />
+                    </List.Item>
+                )}
+            />
+        );
     }
 }
 
-export default TaskCreate;
+class MessageWindow extends Component {
+    state = {
+        messages: [
+            new Message({id: 0, message: 'Hello!'}),
+            new Message({id: 1, message: 'World!'}),
+        ],
+    };
+
+    render() {
+        return (
+            <Layout>
+                <Header style={{background: '#fff'}}>Who</Header>
+                <Content style={{background: '#fff'}}>
+                    <ChatFeed
+                        messages={this.state.messages}
+                        showSenderName
+                        bubblesCentered={false}
+                        maxHeight={512}
+                        bubbleStyles={
+                            {
+                                text: {
+                                    fontSize: 14,
+                                },
+                                chatbubble: {
+                                    borderRadius: 50,
+                                    padding: 10,
+                                },
+                            }
+                        }
+                    />
+                </Content>
+                <Footer style={{background: '#fff'}}>
+                    <TextArea autosize={{minRows: 2, maxRows: 6}}/>
+                    <MessageButton>发送</MessageButton>
+                </Footer>
+            </Layout>
+        );
+    }
+}
+
+const borderStyle = {
+    background: '#fff',
+    'border-style': 'solid',
+    'border-color': 'lightgray',
+    'border-width': '1px',
+};
+
+class TaskIM extends Component {
+    render() {
+        return (
+            <Layout>
+                <Sider style={borderStyle}><ContactList/></Sider>
+                <Content style={borderStyle}><MessageWindow/></Content>
+            </Layout>
+        );
+    }
+}
+
+export default TaskIM;
